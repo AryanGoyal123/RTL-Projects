02a3c35650ecb3d3b19efefc09c028c066c0da70
Aryan Goyal <90419056+AryanGoyal123@users.noreply.github.com>
2025-02-25 23:18:45 -0500

    Update rca.v

--- RCA/rca.v
+++ RCA/rca.v
@@ -32,3 +32,24 @@ module rca_4bit #(parameter WIDTH = 4) (
   full_adder fa4(A[3], B[3], c[2], sum[3], carry_out);
   
 endmodule
+
+module rca_8bit #(parameter WIDTH = 8) (
+  input [WIDTH-1:0] A, B,
+  input carry_in,
+  output carry_out,
+  output [WIDTH-1:0] sum
+);
+
+    wire [WIDTH-1:0] c; // Carry signals between full adders
+
+    // Instantiate full adders for each bit
+    full_adder fa0(A[0], B[0], carry_in, sum[0], c[0]);
+    full_adder fa1(A[1], B[1], c[0], sum[1], c[1]);
+    full_adder fa2(A[2], B[2], c[1], sum[2], c[2]);
+    full_adder fa3(A[3], B[3], c[2], sum[3], c[3]);
+    full_adder fa4(A[4], B[4], c[3], sum[4], c[4]);
+    full_adder fa5(A[5], B[5], c[4], sum[5], c[5]);
+    full_adder fa6(A[6], B[6], c[5], sum[6], c[6]);
+    full_adder fa7(A[7], B[7], c[6], sum[7], carry_out);
+
+endmodule
